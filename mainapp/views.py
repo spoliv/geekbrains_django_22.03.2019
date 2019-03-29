d95c6bb4a5b47da9995d0507829df25a900fe21c
OlegSP <spoliv@rambler.ru>
2019-03-29 10:08:06 +0300

    Дополнил подшаблон основного меню

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,14 +2,27 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html', context={'title': 'home'})
+    return render(request, 'mainapp/index.html',
+    context={'title': 'home', 'main_menu': [{'href': 'index', 'name': 'home'},
+                                            {'href': 'catalog', 'name': 'products'},
+                                            {'href': 'contacts', 'name': 'contact'}
+                                            ]})
+
 
 
 def products(request):
-    return render(request, 'mainapp/catalog.html', context={'title': 'catalog'})
+    return render(request, 'mainapp/catalog.html',
+    context={'title': 'catalog', 'main_menu': [{'href': 'index', 'name': 'home'},
+                                            {'href': 'catalog', 'name': 'products'},
+                                            {'href': 'contacts', 'name': 'contact'}
+                                            ]})
 
 
 def contact(request):
-    return render(request, 'mainapp/contacts.html', context={'title': 'contacts'})
+    return render(request, 'mainapp/contacts.html',
+    context={'title': 'contacts', 'main_menu': [{'href': 'index', 'name': 'home'},
+                                            {'href': 'catalog', 'name': 'products'},
+                                            {'href': 'contacts', 'name': 'contact'}
+                                            ]})
 
 # Create your views here.
