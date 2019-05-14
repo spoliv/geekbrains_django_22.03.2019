b3c3de7d12a64dbe0be9512bec4129b251bd91d8
OlegSP <spoliv@rambler.ru>
2019-05-14 08:46:09 +0300

    Django2_Lesson_3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'^__debug__/', include(debug_toolbar.urls)),
     url(r'^social/', include('social_django.urls', namespace='social')),
+    url(r'^zakaz/', include('ordersapp.urls', namespace='zakaz')),
 
 ]
 
